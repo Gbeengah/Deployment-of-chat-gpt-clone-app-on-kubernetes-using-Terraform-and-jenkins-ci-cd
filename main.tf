f1a615785e65815513f302b456fd87357f89fd9c
Gbeengah <97206259+Gbeengah@users.noreply.github.com>
2024-08-30 19:02:50 +0100

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,130 +1,58 @@
-# Define IAM Policy Document for EKS Cluster Role
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
+resource "aws_security_group" "Jenkins-sg" {
+  name        = "Jenkins-Security Group"
+  description = "Open 22,443,80,8080,9000,9100,9090,3000"
 
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
+  # Define a single ingress rule to allow traffic on all specified ports
+  ingress = [
+    for port in [22, 80, 443, 8080, 9000,9100,9090,3000] : {
+      description      = "TLS from VPC"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
     }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-# IAM Role for EKS Cluster
-resource "aws_iam_role" "example" {
-  name               = "eks-cluster-cloud"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-# Attach the necessary policies to the EKS Cluster Role
-resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.example.name
-}
-
-# Get default VPC data
-data "aws_vpc" "default" {
-  default = true
-}
-
-# Get specific public subnets in supported availability zones
-data "aws_subnet" "subnet_a" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-
-  filter {
-    name   = "availability-zone"
-    values = ["us-east-1a"]
-  }
-}
-
-data "aws_subnet" "subnet_b" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-
-  filter {
-    name   = "availability-zone"
-    values = ["us-east-1b"]
-  }
-}
-
-# Define a local variable to collect supported subnets into a list
-locals {
-  supported_subnets = [
-    data.aws_subnet.subnet_a.id,
-    data.aws_subnet.subnet_b.id,
   ]
-}
 
-# EKS Cluster Provision
-resource "aws_eks_cluster" "example" {
-  name     = "EKS_CLOUD"
-  role_arn = aws_iam_role.example.arn
-
-  vpc_config {
-    subnet_ids = local.supported_subnets
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
-  ]
-}
-
-# IAM Role for EKS Node Group
-resource "aws_iam_role" "example1" {
-  name = "eks-node-group-cloud"
-
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
-}
-
-# Attach the necessary policies to the EKS Node Group Role
-resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.example1.name
-}
-
-resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.example1.name
-}
-
-resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.example1.name
-}
-
-# Create Node Group
-resource "aws_eks_node_group" "example" {
-  cluster_name    = aws_eks_cluster.example.name
-  node_group_name = "Node-cloud"
-  node_role_arn   = aws_iam_role.example1.arn
-  subnet_ids      = local.supported_subnets
-
-  scaling_config {
-    desired_size = 1
-    max_size     = 2
-    min_size     = 1
+  tags = {
+    Name = "Jenkins-sg"
   }
-  instance_types = ["t2.medium"]
+}
 
-  depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
-  ]
+
+resource "aws_instance" "web" {
+  ami                    = "ami-0c7217cdde317cfec"  #change your ami value according to your aws instance
+  instance_type          = "t2.large"
+  key_name               = "my key"
+  vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
+  user_data              = templatefile("./script.sh", {})
+
+  tags = {
+    Name = "gpt clone"
+  }
+  root_block_device {
+    volume_size = 30
+  }
+}
+resource "aws_instance" "web2" {
+  ami                    = "ami-0c7217cdde317cfec" #change your ami value according to your aws instance 
+  instance_type          = "t2.medium"
+  key_name               = "my key"
+  vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
+  tags = {
+    Name = "Monitering via grafana"
+  }
+  root_block_device {
+    volume_size = 30
+  }
 }
